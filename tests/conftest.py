4ead89e2085e5b0bbbd81e3b5f8a934d23835ca0
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 00:51:28 +0300

    upd

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,25 @@
-import os
-
 import pytest
-from dotenv import load_dotenv
-from selene import Browser, Config
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
 from utils import attach
-
-
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
-
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
-
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "100.0")
-    options.set_capability("selenoid:options", {
-        "enableVNC": True,
-        "enableVideo": True
-    })
-
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
