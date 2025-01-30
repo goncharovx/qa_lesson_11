65d9eb043d601a2c55d745b79bfe657207150999
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 16:08:31 +0300

    upd

--- utils/attach.py
+++ utils/attach.py
@@ -4,7 +4,7 @@ from allure_commons.types import AttachmentType
 
 
 def add_screenshot(browser):
-    png = browser.driver.get_screenshot_as_png()  # Используем browser.driver для получения скриншота
+    png = browser.get_screenshot_as_png()
     allure.attach(
         body=png,
         name="screenshot",
@@ -14,17 +14,22 @@ def add_screenshot(browser):
 
 
 def add_logs(browser):
-    logs = "".join(f"{entry['level']}: {entry['message']}\n" for entry in browser.driver.get_log("browser"))  # Используем browser.driver
-    allure.attach(
-        logs,
-        name="browser_logs",
-        attachment_type=AttachmentType.TEXT,
-        extension=".log"
-    )
+    """Добавляет логи браузера в Allure"""
+    try:
+        logs = "".join(f"{entry['level']}: {entry['message']}\n" for entry in browser.get_log("browser"))
+        allure.attach(
+            logs,
+            name="browser_logs",
+            attachment_type=AttachmentType.TEXT,
+            extension=".log"
+        )
+    except Exception as e:
+        allure.attach(str(e), name="log_error", attachment_type=AttachmentType.TEXT)
 
 
 def add_html(browser):
-    html = browser.driver.page_source  # Используем browser.driver для получения HTML-кода
+    """Добавляет HTML-код страницы в Allure"""
+    html = browser.page_source
     allure.attach(
         html,
         name="page_source",
@@ -34,6 +39,7 @@ def add_html(browser):
 
 
 def add_video(browser):
-    video_url = f"https://{os.getenv('SELENOID_URL')}/video/{browser.driver.session_id}.mp4"
+    """Добавляет ссылку на видео из Selenoid"""
+    video_url = f"https://{os.getenv('SELENOID_URL')}/video/{browser.session_id}.mp4"
     html = f"<video width='100%' height='100%' controls autoplay><source src='{video_url}' type='video/mp4'></video>"
     allure.attach(html, name="video", attachment_type=AttachmentType.HTML)
