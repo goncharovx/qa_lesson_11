ff1af5b33c529e07a480f40a65472b9b88a0f573
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 13:29:02 +0300

    upd

--- conftest.py
+++ conftest.py
@@ -1,30 +1,33 @@
 import os
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import pytest
-from dotenv import load_dotenv
+from selene import Browser, Config
 
-# Загружаем переменные окружения из .env
-load_dotenv()
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def setup_browser():
     options = Options()
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "125.0",
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.set_capability("selenoid:options", selenoid_capabilities)
+
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
-    yield driver
+
+    browser = Browser(Config(driver=driver))  # Привязываем драйвер к selene
+    yield browser
     driver.quit()
