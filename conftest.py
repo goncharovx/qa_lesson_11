241a3f3c5d5945c7d6a59687f5d679aefd920692
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 13:45:47 +0300

    add some actions to detect errors

--- conftest.py
+++ conftest.py
@@ -3,7 +3,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
+from dotenv import load_dotenv
 
+if not os.path.exists(".env"):
+    raise FileNotFoundError("Файл .env не найден в рабочей директории")
+
+load_dotenv()
+
+print("SELENOID_LOGIN:", os.getenv("SELENOID_LOGIN"))
+print("SELENOID_PASS:", os.getenv("SELENOID_PASS"))
+print("SELENOID_URL:", os.getenv("SELENOID_URL"))
 
 @pytest.fixture(scope="session")
 def setup_browser():
