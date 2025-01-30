dcb70031f7030ea90fef86ca37b1e2334d4b3dcf
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 16:41:19 +0300

    del config

--- conftest.py
+++ conftest.py
@@ -2,54 +2,17 @@ import os
 
 import pytest
 from dotenv import load_dotenv
-from selene import Config, Browser, browser
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 from utils import attach
 
 if not os.path.exists(".env"):
     raise FileNotFoundError("Файл .env не найден в рабочей директории")
 
-# load_dotenv()
-
-
-# @pytest.fixture(scope="session")
-# def setup_browser():
-#     options = Options()
-#
-#     selenoid_login = os.getenv("SELENOID_LOGIN")
-#     selenoid_pass = os.getenv("SELENOID_PASS")
-#     selenoid_url = os.getenv("SELENOID_URL")
-#
-#     if not selenoid_url or selenoid_url.lower() in ["none", ""]:
-#         raise ValueError(f"Переменная SELENOID_URL не задана! Текущее значение: {selenoid_url}")
-#
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "125.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#
-#     options.set_capability("selenoid:options", selenoid_capabilities)
-#
-#     service = Service(ChromeDriverManager().install())
-#
-#     driver = webdriver.Remote(
-#         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-#         options=options
-#     )
-#
-#     browser = Browser(Config(driver=driver))
-#     yield browser
-#     driver.quit()
-
-
 DEFAULT_BROWSER_VERSION = "126.0"
+
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -57,9 +20,11 @@ def pytest_addoption(parser):
         default=DEFAULT_BROWSER_VERSION,
     )
 
+
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
+
 
 @pytest.fixture(scope='function', autouse=True)
 def open_browser(request):
