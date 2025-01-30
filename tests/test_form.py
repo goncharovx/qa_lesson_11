4bb47a22e5950700254b4679802536b7b8f9ab69
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 12:40:39 +0300

    add attachments in test

--- tests/test_form.py
+++ tests/test_form.py
@@ -3,10 +3,10 @@ import os
 import allure
 from selene import Browser, Config, have
 from selene.api import s
+from utils import attach
 
 current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, 'resources', 'pic.png')
-
 
 def test_registration_form(setup_browser):
     browser = Browser(Config(driver=setup_browser))
@@ -17,47 +17,64 @@ def test_registration_form(setup_browser):
         browser.open('https://demoqa.com/automation-practice-form')
         browser.driver.execute_script("$('footer').remove()")
         browser.driver.execute_script("$('#fixedban').remove()")
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
 
     with allure.step('Заполнить ФИО'):
         s('#firstName').type(first_name)
         s('#lastName').type(last_name)
+        attach.add_screenshot(browser)
 
     with allure.step('Заполнить поле ввода e-mail'):
         s('#userEmail').type('test@mail.ru')
+        attach.add_screenshot(browser)
 
     with allure.step('Выбрать пол'):
         s('[for="gender-radio-1"]').click()
+        attach.add_screenshot(browser)
 
     with allure.step('Заполнить поле ввода телефон'):
         s('#userNumber').type('9939993388')
+        attach.add_screenshot(browser)
 
     with allure.step('Установить дату рождения'):
         s('#dateOfBirthInput').click()
         s('.react-datepicker__month-select').click().s('[value="2"]').click()
         s('.react-datepicker__year-select').click().s('[value="1960"]').click()
         s('.react-datepicker__day--003:not(.react-datepicker__day--outside-month)').click()
+        attach.add_screenshot(browser)
 
     with allure.step('Выбрать предмет'):
         s('#subjectsInput').type('Maths').press_enter()
+        attach.add_screenshot(browser)
 
     with allure.step('Выбрать хобби'):
         s('[for="hobbies-checkbox-1"]').click()
         s('[for="hobbies-checkbox-3"]').click()
+        attach.add_screenshot(browser)
 
     with allure.step('Загрузить файл'):
         s('#uploadPicture').send_keys(file_path)
+        attach.add_screenshot(browser)
 
     with allure.step('Заполнить поле ввода "адрес"'):
         s('#currentAddress').type('Moscow 5')
+        attach.add_screenshot(browser)
 
     with allure.step('Выбрать штат'):
         s('#state').click().s('#react-select-3-option-0').click()
+        attach.add_screenshot(browser)
 
     with allure.step('Выбрать город'):
         s('#city').click().s('#react-select-4-option-0').click()
+        attach.add_screenshot(browser)
 
     with allure.step('Подтвердить регистрацию'):
         s('#submit').click()
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
 
     with allure.step('Проверить форму регистрации'):
         s('.table-responsive').should(have.text(first_name + ' ' + last_name))
@@ -71,3 +88,7 @@ def test_registration_form(setup_browser):
         s('.table-responsive').should(have.text('pic.png'))
         s('.table-responsive').should(have.text('Moscow 5'))
         s('.table-responsive').should(have.text('NCR Delhi'))
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
+        attach.add_video(browser)
