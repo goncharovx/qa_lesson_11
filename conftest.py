292603d441157ca58eec45d549f2044c782d662a
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 12:35:21 +0300

    upd

--- conftest.py
+++ conftest.py
@@ -1,43 +1,29 @@
-import pytest
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+import pytest
+from dotenv import load_dotenv
 
+# Загружаем переменные окружения из .env
+load_dotenv()
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
-# @pytest.fixture(scope='function')
-# def setup_browser(request):
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "126.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True,
-#         }
-#     }
-#
-#     options.capabilities.update(selenoid_capabilities)
-#     driver = webdriver.Remote(
-#         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#     browser = Browser(Config(driver=driver))
-#     yield browser
-#
-#     browser.quit()
-
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def setup_browser():
-    options = webdriver.ChromeOptions()
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "125.0")
-    options.set_capability("selenoid:options", {
-        "enableVNC": True,
-        "enableVideo": True,
-    })
-
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",  # Проверьте доступную версию
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.set_capability("selenoid:options", selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
     yield driver
