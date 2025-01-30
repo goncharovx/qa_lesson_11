bdc9e88a997cd2ac7539fb0ee452339239bf4ea5
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-30 16:11:37 +0300

    upd

--- utils/attach.py
+++ utils/attach.py
@@ -4,7 +4,8 @@ from allure_commons.types import AttachmentType
 
 
 def add_screenshot(browser):
-    png = browser.get_screenshot_as_png()
+    """Добавляет скриншот в Allure"""
+    png = browser.driver.get_screenshot_as_png()
     allure.attach(
         body=png,
         name="screenshot",
@@ -16,7 +17,7 @@ def add_screenshot(browser):
 def add_logs(browser):
     """Добавляет логи браузера в Allure"""
     try:
-        logs = "".join(f"{entry['level']}: {entry['message']}\n" for entry in browser.get_log("browser"))
+        logs = "".join(f"{entry['level']}: {entry['message']}\n" for entry in browser.driver.get_log("browser"))
         allure.attach(
             logs,
             name="browser_logs",
@@ -29,7 +30,7 @@ def add_logs(browser):
 
 def add_html(browser):
     """Добавляет HTML-код страницы в Allure"""
-    html = browser.page_source
+    html = browser.driver.page_source
     allure.attach(
         html,
         name="page_source",
@@ -40,6 +41,6 @@ def add_html(browser):
 
 def add_video(browser):
     """Добавляет ссылку на видео из Selenoid"""
-    video_url = f"https://{os.getenv('SELENOID_URL')}/video/{browser.session_id}.mp4"
+    video_url = f"https://{os.getenv('SELENOID_URL')}/video/{browser.driver.session_id}.mp4"
     html = f"<video width='100%' height='100%' controls autoplay><source src='{video_url}' type='video/mp4'></video>"
     allure.attach(html, name="video", attachment_type=AttachmentType.HTML)
